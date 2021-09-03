4d0957ee1d7bb0cd77a7a829f309729aa6d9d118
gabriek0 <gabriel.sanches170@gmail.com>
2021-09-03 11:22:49 -0300

    first commit

---
+++ readme.md
@@ -0,0 +1,35 @@
+<h1 align="center"> Cause Effect App 🖱️ </h1>
+
+<p align="center">
+  <a href="Sobre-o-projeto">Projeto</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#tecnologias">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#Funcionalidades">Funcionalidades</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#autor">Autor</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#licença">Licença</a>
+</p>
+
+### 💡 Projeto
+
+<p align='center'>Uma lista de nomes de pessoas. Assim, ao clicar sobre os nomes é mostrado algumas informações pessoais.</p>
+
+### ⚙️ Tecnologias
+
+- [Javascript](https://developer.mozilla.org/pt-BR/docs/Web/JavaScript)
+- [CSS](https://developer.mozilla.org/pt-BR/docs/Web/CSS)
+- [HTML](https://developer.mozilla.org/pt-BR/docs/Web/HTML)
+
+
+### 🔨 Funcionalidades
+ - [x] O usuário pode ver uma lista de nomes de pessoas organizados verticalmente em um painel de resumo na página.
+ - [x] O usuário pode clicar em um nome na lista para atualizar um painel adjacente na página com o nome completo, endereço, número de telefone e aniversário desse indivíduo.
+ - [x] O usuário pode clicar em outro nome na lista para atualizar o painel de detalhes com as informações desse indivíduo.
+
+
+###  Autor
+
+ 👤 **Gabriel Henrique**
+
+  - Github: [@Gabriek0](https://github.com/Gabriek0)
+- LinkedIn: [@gabriel-henrique-664bb219a](https://www.linkedin.com/in/gabriel-henrique-664bb219a/)
+
+### 📜 Licença
--- src/js/main.js
+++ src/js/main.js
@@ -1,86 +1,188 @@
-const peopleContainer = document.querySelector('.peopleContainer')
+const informations = document.querySelector('.informationsContainer');
 
-const peopleInformations = [
+const peoples = [
   {
-    name: 'Rodrigo',
-    fullName: 'Rodrigo dos Santos',
-    street: 'Paulista',
-    city: 'São Paulo',
-    state: 'SP',
-    country: 'Brazil',
-    telephone: '707070',
-    birthday: '17/10/2001'
+    name: 'Filipe Alexandre',
+    fullName: 'Filipe Alexandre Murilo Ramos',
+    street: 'Rua José Barbosa, 5430',
+    city: 'Auriflama',
+    state: 'São Paulo',
+    country: 'Brasil',
+    telephone: '+55 (17) 98551-9936',
+    birthday: '22/11/1996',
   },
 
   {
-    name: 'Rodrigo',
-    fullName: 'Rodrigo dos Santos',
-    street: 'Paulista',
-    city: 'São Paulo',
-    state: 'SP',
-    country: 'Brazil',
-    telephone: '707070',
-    birthday: '17/10/2001'
+    name: 'Renata Sueli',
+    fullName: 'Renata Sueli Marina Mendes',
+    street: 'Avenida Doutor Carlos Botelho, 403',
+    city: 'Pariquera-Açú',
+    state: 'São Paulo',
+    country: 'Brasil',
+    telephone: '+55 (13) 98253-0930',
+    birthday: '24/08/2003',
   },
 
-]
+  {
+    name: 'Antonella Louise',
+    fullName: 'Antonella Louise Carvalho',
+    street: 'Parque Represa Billings III',
+    city: 'Santo André',
+    state: 'São Paulo',
+    country: 'Brasil',
+    telephone: '+55 (11) 99180-3464',
+    birthday: '15/08/2003',
+  },
 
-function start() {
+  {
+    name: 'Leonardo Thomas',
+    fullName: 'Leonardo Thomas Davi Aparício',
+    street: 'Rua Quatro',
+    city: 'Santos',
+    state: 'São Paulo',
+    country: 'Brasil',
+    telephone: '+55 (13) 99510-5452',
+    birthday: '07/08/1994',
+  }
+];
 
-  peopleInformations.map(people => {
-    const allPeoples = document.createElement('div')
-    allPeoples.classList.add('allPeoples')
+peoples.map(people => {
+  const rightArrow = document.createElement('img');
+  rightArrow.src = './img/right-arrow.png';
+  rightArrow.className = 'rightArrow';
 
-    const informations = document.createElement('div');
-    informations.classList.add('informations')
+  //Personal containers
+  const fullNameContainer = document.createElement('div');
+  const streetContainer = document.createElement('div');
+  const cityContainer = document.createElement('div');
+  const stateContainer = document.createElement('div');
+  const countryContainer = document.createElement('div');
+  const telephoneContainer = document.createElement('div');
+  const birthdayContainer = document.createElement('div');
 
-    const names = document.createElement('p');
-    names.classList.add('name')
+  //Personal details
+  const name = document.createElement('p');
+  const fullName = document.createElement('p');
+  const street = document.createElement('p');
+  const city = document.createElement('p');
+  const state = document.createElement('p');
+  const country = document.createElement('p');
+  const telephone = document.createElement('p');
+  const birthday = document.createElement('p');
 
-    const fullName = document.createElement('p');
-    const street = document.createElement('p');
-    const city = document.createElement('p');
-    const state = document.createElement('p');
-    const country = document.createElement('p');
-    const telephone = document.createElement('p');
-    const birthday = document.createElement('p');
-
-
-    names.addEventListener('click', () => {
-      const content = allPeoples.contains(informations)
-
-      if (content) {
-        informations.remove();
-      } else {
-
-        allPeoples.appendChild(informations)
-        informations.appendChild(fullName)
-        informations.appendChild(street)
-        informations.appendChild(city)
-        informations.appendChild(state)
-        informations.appendChild(country)
-        informations.appendChild(telephone)
-        informations.appendChild(birthday)
-
-
-        fullName.textContent = `Full-name: ${people.fullName}`;
-        street.textContent = `Street: ${people.street}`;
-        city.textContent = `City: ${people.city}`;
-        state.textContent = `State: ${people.state}`;
-        country.textContent = `Country: ${people.country}`;
-        telephone.textContent = `Telephone: ${people.telephone}`;
-        birthday.textContent = `Birthday ${people.birthday}`;
-
-      }
-    })
+  // information title
+  const fullNameTitle = document.createElement('p');
+  const streetTitle = document.createElement('p');
+  const cityTitle = document.createElement('p');
+  const stateTitle = document.createElement('p');
+  const countryTitle = document.createElement('p');
+  const telephoneTitle = document.createElement('p');
+  const birthdayTitle = document.createElement('p');
 
 
-    peopleContainer.appendChild(allPeoples);
-    allPeoples.appendChild(names)
-    names.textContent = people.name
+  const shortNameContainer = document.createElement('div')
+  const peopleDetails = document.createElement('div');
+
+  //Content
+  name.textContent = people.name;
+  fullName.textContent = people.fullName;
+  street.textContent = people.street;
+  city.textContent = people.city;
+  state.textContent = people.state;
+  country.textContent = people.country;
+  telephone.textContent = people.telephone;
+  birthday.textContent = people.birthday;
+
+
+  //Content titles
+  fullNameTitle.textContent = 'Name: ';
+  streetTitle.textContent = 'Street: ';
+  cityTitle.textContent = 'City: ';
+  stateTitle.textContent = 'State: ';
+  countryTitle.textContent = 'Country: ';
+  telephoneTitle.textContent = 'Telephone: ';
+  birthdayTitle.textContent = 'Birthday: ';
+
+  //Container title
+  fullNameContainer.appendChild(fullNameTitle);
+  stateContainer.appendChild(stateTitle);
+  cityContainer.appendChild(cityTitle);
+  stateContainer.appendChild(stateTitle);
+  countryContainer.appendChild(countryTitle);
+  telephoneContainer.appendChild(telephoneTitle);
+  birthdayContainer.appendChild(birthdayTitle);
+
+  //append details into containers
+  fullNameContainer.appendChild(fullName)
+  streetContainer.appendChild(street);
+  cityContainer.appendChild(city);
+  stateContainer.appendChild(state);
+  countryContainer.appendChild(country);
+  telephoneContainer.appendChild(telephone);
+  birthdayContainer.appendChild(birthday);
+
+  // name initial
+  shortNameContainer.appendChild(name);
+  shortNameContainer.appendChild(rightArrow)
+
+  // show on screen
+  informations.appendChild(shortNameContainer);
+  informations.appendChild(peopleDetails);
+
+  // peopleDetails hidden
+  peopleDetails.style.display = 'none'
+
+  shortNameContainer.addEventListener('click', () => {
+    if (peopleDetails.style.display === 'none') {
+      peopleDetails.style.display = 'block';
+      peopleDetails.style.animation = 'move 0.5s'
+      rightArrow.src = './img/arrow-down.png';
+
+    }
+    else {
+      peopleDetails.style.animation = 'toRecall 0.3s';
+
+      setTimeout(() => {
+        peopleDetails.style.display = 'none';
+        rightArrow.src = './img/right-arrow.png';
+
+      }, 300)
+    }
   })
-};
 
-start();
+  peopleDetails.appendChild(fullNameContainer);
+  peopleDetails.appendChild(stateContainer);
+  peopleDetails.appendChild(cityContainer);
+  peopleDetails.appendChild(stateContainer);
+  peopleDetails.appendChild(countryContainer);
+  peopleDetails.appendChild(telephoneContainer);
+  peopleDetails.appendChild(birthdayContainer);
 
+  shortNameContainer.className = 'shortNameContainer';
+  peopleDetails.className = 'peopleDetails';
 
+  fullNameContainer.className = 'line';
+  streetContainer.className = 'line';
+  cityContainer.className = 'line';
+  stateContainer.className = 'line';
+  countryContainer.className = 'line';
+  telephoneContainer.className = 'line';
+  birthdayContainer.className = 'line';
+
+  name.className = 'shortName'
+  fullName.className = 'info';
+  street.className = 'info';
+  city.className = 'info';
+  state.className = 'info';
+  country.className = 'info';
+  telephone.className = 'info';
+  birthday.className = 'info';
+
+  fullNameTitle.className = 'infoTitle';
+  streetTitle.className = 'infoTitle';
+  cityTitle.className = 'infoTitle';
+  stateTitle.className = 'infoTitle';
+  countryTitle.className = 'infoTitle';
+  telephoneTitle.className = 'infoTitle';
+  birthdayTitle.className = 'infoTitle';
+});
